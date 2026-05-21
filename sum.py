762efb8e05705db50657608ba41191cf54ed292e
sigma-demo <prayassaini180805@gmail.com>
2026-05-21 11:01:50 +0530

    Arithmetic_Operators

---
+++ sum.py
@@ -0,0 +1,16 @@
+def sum(a, b):
+    return(a + b)
+
+def sub(a, b):
+    return(a - b)
+
+def multi(a, b):
+    return(a * b)
+
+def div(a, b):
+    return(a/b)
+
+print(sum(2, 3))
+print(sub(34, 23))
+print(multi(12, 11))
+print(div(12, 5))
